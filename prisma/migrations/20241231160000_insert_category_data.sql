f3c09350adec3ca1af1b9e2ae51e3391ec5c15c3
swornim2 <sswornimm2@gmail.com>
2024-12-31 16:47:23 +0545

    Uploading the migration

---
+++ prisma/migrations/20241231160000_insert_category_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO Category (id, name, slug, description, imagePath, createdAt, updatedAt)
+VALUES
+('93bd8058-6bbe-4f02-9822-0cdab1e830a1', 'electronics', 'electronics', 'new gadgets', '/categories/electronics-1735635912937.jpg', 1735635912701, 1735635912939);
